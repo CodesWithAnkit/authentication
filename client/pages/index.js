94596eec91e04919136e705031f712cde67f4bff
codeswithankit <ank8804465648@gmail.com>
2021-04-13 00:41:13 +0530

    Next js app setup by self and installed bootstrap and antd package antd is a metrial ui like package

---
+++ client/pages/index.js
@@ -0,0 +1,12 @@
+const Index = () => {
+  return (
+    <>
+      <h1 className="jumbotron text-center bg-primary square">
+        Self setup nextjs app
+      </h1>
+      <p>It is setup by me while looking the docs</p>
+    </>
+  );
+};
+
+export default Index;
---
+++ client/pages/register.js
@@ -0,0 +1,9 @@
+const Register = () => {
+  return (
+    <>
+      <h1 className="jumbotron bg-primary square">Register</h1>
+    </>
+  );
+};
+
+export default Register;
